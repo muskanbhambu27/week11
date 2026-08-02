3424d732e0cbd8db76b1def4d9334bbeded9b16f
muskanbhambu27 <163241099+muskanbhambu27@users.noreply.github.com>
2026-08-02 13:22:26 -0400

    Fix security group to pass tfsec scan

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "web-sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
