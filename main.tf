771b6529aed5cd11e16181a08e439e5d9c16f0e7
muskanbhambu27 <163241099+muskanbhambu27@users.noreply.github.com>
2026-08-02 13:31:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     aws = {
@@ -13,43 +12,33 @@ terraform {
     }
   }
   required_version = ">= 1.1.0"
-
   cloud {
     organization = "REPLACE_ME"
-
     workspaces {
       name = "gh-actions-demo"
     }
   }
 }
-
 provider "aws" {
   region = "us-west-2"
 }
-
 resource "random_pet" "sg" {}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"] # Canonical
 }
-
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
@@ -58,25 +47,37 @@ resource "aws_instance" "web" {
               echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
               EOF
-}
 
+  root_block_device {
+    encrypted = true
+  }
+
+  metadata_options {
+    http_tokens = "required"
+  }
+}
 resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+  name        = "${random_pet.sg.id}-sg"
+  description = "Allow inbound web traffic on port 8080 from internal network"
+
   ingress {
+    description = "Allow HTTP traffic from internal network"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
+
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
+    description = "Allow outbound internet access for package installation"
+    #tfsec:ignore:aws-vpc-no-public-egress-sgr
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
